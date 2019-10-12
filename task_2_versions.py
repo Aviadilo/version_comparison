125f2ed5246a2422a0fc6e49f8bbad2e64671122
Hanna Lychauka <zuzga@tut.by>
2019-10-12 22:26:59 +0300

    methods for parsing input data

--- task_2_versions.py
+++ task_2_versions.py
@@ -1,9 +1,11 @@
 import functools
+import re
 
 
 @functools.total_ordering
 class Version:
     def __init__(self, version):
+        version = make_value(version)
         self.version = version
 
     def __eq__(self, other):
@@ -11,6 +13,21 @@ class Version:
 
     def __lt__(self, other):
         return self.version < other
+
+
+def make_value(raw_str):
+    if not re.search('[a-zA-Z]', raw_str):
+        raw_str = raw_str + '.z'
+    splited = re.split('\.|-', raw_str)
+    splited = make_int(splited)
+    return splited
+
+
+def make_int(our_list):
+    for i in range(len(our_list)):
+        if our_list[i].isdigit():
+            our_list[i] = int(our_list[i])
+    return our_list
 
 
 def main():
@@ -24,7 +41,6 @@ def main():
     ]
 
     for version_1, version_2 in to_test:
-        print(version_1, version_2)
         assert Version(version_1) < Version(version_2), 'le failed'
         assert Version(version_2) > Version(version_1), 'ge failed'
         assert Version(version_2) != Version(version_1), 'neq failed'
